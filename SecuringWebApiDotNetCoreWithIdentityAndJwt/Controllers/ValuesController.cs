855bc58e71cbcfe4cf8373dbaa4aa25daab56d54
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-12-03 22:46:12 +0330

    Add GetToken

--- SecuringWebApiDotNetCoreWithIdentityAndJwt/Controllers/ValuesController.cs
+++ SecuringWebApiDotNetCoreWithIdentityAndJwt/Controllers/ValuesController.cs
@@ -1,7 +1,14 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SecuringWebApiDotNetCoreWithIdentityAndJwt.Models;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SecuringWebApiDotNetCoreWithIdentityAndJwt.Controllers
 {
@@ -14,7 +21,7 @@ namespace SecuringWebApiDotNetCoreWithIdentityAndJwt.Controllers
         [AllowAnonymous]
         public ActionResult GetFruits()
         {
-            List<string> mylist = new List<string>() {"apples","bannanas" };
+            List<string> mylist = new List<string>() { "apples", "bannanas" };
             return Ok(mylist);
         }
 
@@ -23,6 +30,34 @@ namespace SecuringWebApiDotNetCoreWithIdentityAndJwt.Controllers
         {
             List<string> mylist = new List<string>() { "organic apples", "organic bannanas" };
             return Ok(mylist);
+        }
+        [AllowAnonymous]
+        [HttpPost("getToken")]
+        public async Task<ActionResult> GetToken([FromBody] MyLoginModelType myLoginModelType)
+        {
+            if (myLoginModelType.Email == "javad.jahangiri.niopdc@gmail.com" && myLoginModelType.Password=="Pa$sw0rd")
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var key= Encoding.ASCII.GetBytes("@#MY_BIG_SECRET_KEY@#");
+                var tokenDescription = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, myLoginModelType.Email)
+                    }
+                   ),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                };
+            var token = tokenHandler.CreateToken(tokenDescription);
+            var tokenString = tokenHandler.WriteToken(token);
+            return Ok(new { token = tokenString } );
+            }
+            else
+            {
+                return Unauthorized("try again");
+            }
+
         }
     }
 }
