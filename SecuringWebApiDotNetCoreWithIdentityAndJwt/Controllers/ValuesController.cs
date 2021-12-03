1a600172a2dc6144316c059750db293fafe9502a
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-12-03 20:44:19 +0330

    Add Sample Controller Authenticated

---
+++ SecuringWebApiDotNetCoreWithIdentityAndJwt/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace SecuringWebApiDotNetCoreWithIdentityAndJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("getFruits")]
+        [AllowAnonymous]
+        public ActionResult GetFruits()
+        {
+            List<string> mylist = new List<string>() {"apples","bannanas" };
+            return Ok(mylist);
+        }
+
+        [HttpGet("getFruitsAuthenticated")]
+        public ActionResult GetFruitsAuthenticated()
+        {
+            List<string> mylist = new List<string>() { "organic apples", "organic bannanas" };
+            return Ok(mylist);
+        }
+    }
+}
