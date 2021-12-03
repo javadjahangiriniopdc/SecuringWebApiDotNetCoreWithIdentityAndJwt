49cbb521cc53fd887fe9b5a01c47dca72fbb0fc7
drjj007 <javad.jahangiri.niopdc@gmail.com>
2021-12-03 20:52:36 +0330

    add MyLoginModelType

---
+++ SecuringWebApiDotNetCoreWithIdentityAndJwt/Models/MyLoginModelType.cs
@@ -0,0 +1,9 @@
+﻿namespace SecuringWebApiDotNetCoreWithIdentityAndJwt.Models
+{
+    public class MyLoginModelType
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
